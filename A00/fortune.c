d8f40625cc304009fdb6721b897f0708565cc34e
jamccarty <jmccarty@DESKTOP-9KGPCJM.localdomain>
2022-01-21 13:39:37 -0500

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*Author jamccarty
+ * Created 1/21/2022
+ * Prints a fortune
+ */
+
+int main(){
+	printf("A journey of a thousand miles starts with a single step\n");
+	return 0;
+}
