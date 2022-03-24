e14d6ff4b2ee8cd94b3b0c241980848e598d601e
jamccarty <jmccarty@DESKTOP-9KGPCJM.localdomain>
2022-03-23 20:51:28 -0400

    Revised scramble.c for readability, less clunky

--- J03/scramble.c
+++ J03/scramble.c
@@ -2,36 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-  char *msg = malloc(sizeof(char) * 51);
+int main(){
+  char *msg = malloc(sizeof(char) * 1024);
   printf("Please enter a phrase: ");
-  fgets(msg, 51, stdin);
-
+  fgets(msg, 1023, stdin);
   int len = strlen(msg);
   char second = 0x02;
   char third = 0x04;
-
+  
   for(int i = 0; i < len; i++){
-    char bit2 = second & msg[i];
-    char bit3 = third & msg[i];
-    bit2 = bit2 << 1;
-    bit3 = bit3 >> 1; 
-    
-    if(bit2 == 0x0){
-      char mask = (-1) * third - 1;
-      msg[i] = msg[i] & mask;
-    }else{
-      msg[i] = msg[i] | bit2;
-    }
-
-    if(bit3 == 0x0){
-      char mask = (-1) * second - 1;
-      msg[i] = msg[i] & mask;
-    }else{
-      msg[i] = msg[i] | bit3;
+    if(((msg[i] & second) != second) ^ ((msg[i] & third) != third)){
+      msg[i] = (msg[i] ^ second) ^ third;
     }
   }
-  printf("\nscramble: %s\n", msg);
+  printf("scramble: %s\n", msg);
   free(msg);
   msg = NULL;
   return(0);
